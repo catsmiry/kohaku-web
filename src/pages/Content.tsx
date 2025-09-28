74d5c43922268103c7f0fdedd8ca19630f5e8a55
kohakuto589 <148501199+kohakuto589@users.noreply.github.com>
2025-09-28 18:08:27 +0900

    Update Content.tsx

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -29,7 +29,7 @@ const Home: Component = () => {
 
 {/* About Section */}
         <section id="about" class="mb-8 p-6 bg-white rounded-lg shadow-lg transition-all duration-1000 ease-in-out">
-          <h2 class="text-3xl font-cute text-[#F17C78]">About 東雲琥珀</h2>
+          <h2 class="text-3xl font-cute text-[#F17C78]">Content</h2>
           <p class="mt-4 text-lg text-gray-700">
             東雲琥珀は ミス廃系アイドルである。<br />
             Reliwave P.S の Fediverse 総括マネージャーもしている。<br />
