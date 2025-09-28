78130f547e701727b6d651d2d6e70a689b36158a
kohakuto589 <148501199+kohakuto589@users.noreply.github.com>
2025-09-28 18:04:28 +0900

    Create Content.tsx

---
+++ src/pages/Content.tsx
@@ -0,0 +1,9 @@
+  {/* About Section */}
+        <section id="about" class="mb-8 p-6 bg-white rounded-lg shadow-lg transition-all duration-1000 ease-in-out">
+          <h2 class="text-3xl font-cute text-[#F17C78]">About 東雲琥珀</h2>
+          <p class="mt-4 text-lg text-gray-700">
+            東雲琥珀は ミス廃系アイドルである。<br />
+            Reliwave P.S の Fediverse 総括マネージャーもしている。<br />
+            普段は <a href="https://misskey.gg/@kohaku_shinonome" class="text-blue-500 hover:text-blue-700 transition-colors duration-300">misskey.gg</a> にいるらしい。
+          </p>
+        </section>
